5cadf6625a3dc2e6b48f4b71f2a4cfbb07eeae1b
Stefanivskyi <muxailo717@gmail.com>
2018-02-17 22:21:30 +0200

    fixed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,53 +1,47 @@
 import React, { Component } from 'react';
 import Button from './components/Button';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      bgColor: 'black',
-      Active: 'black'
-    };
-
-    this.changeBackground = this.changeBackground.bind(this)
-  }
-  changeBackground(color) {
-
-    this.setState({
-      Active: color,
-      Animation: true
-    });
-
-    setTimeout(()=>{
-      this.setState({
-        bgColor: color,
-        Animation: false
-
-      })
-    },500)
-  }
 
   render() {
-    let { bgColor, Active, Animation} = this.state;
+    let { activeColor, bgColor, animation} = this.props;
+
     return (
         <div className="App">
-          <div className="main-container" id="main-container"
-               style = {{backgroundColor: bgColor}}>
-            <div className={"scale-container" + [Animation ? ' animation' : '']}
-                 style = {{backgroundColor: Active}}>
+          <div className="main-container"
+               style = {{backgroundColor: bgColor}}
+          >
+            <div className={'scale-container' + [animation ? ' animation' : '']}
+                 style = {{backgroundColor: activeColor}}>
             </div>
           </div>
 
           <div className="colors-btn">
-            <Button color='black'> </Button>
-            <Button color='orange'> </Button>
-            <Button color='green' onClick={() => this.changeBackground('green')}> </Button>
-            <div className={"yellow"  + [Active==='yellow' ? ' active' : '']} onClick={() => this.changeBackground('yellow')}> </div>
-            <div className={"mistyrose"  + [Active==='mistyrose' ? ' active' : '']} onClick={() => this.changeBackground('mistyrose')}> </div>
+            <Button color='black' />
+            <Button color='orange' />
+            <Button color='green' />
+            <Button color='yellow' />
+            <Button color='mistyrose' />
           </div>
         </div>
     );
   }
 }
 
-export default App;
+Button.propTypes = {
+  //redux
+  activeColor: PropTypes.string.isRequired,
+  bgColor: PropTypes.string.isRequired,
+  animation: PropTypes.bool.isRequired,
+};
+
+export default connect(
+    state => ({
+      activeColor: state.activeColor,
+      bgColor: state.bgColor,
+      animation: state.animation
+    }),
+    dispatch => ({})
+)(App);
