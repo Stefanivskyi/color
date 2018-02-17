ae8e04650e99d1b434c8e7e871fa7565c2bc0b5e
Stefanivskyi <muxailo717@gmail.com>
2018-02-17 22:18:16 +0200

    fixed

--- my-app/src/components/Button.jsx
+++ my-app/src/components/Button.jsx
@@ -4,35 +4,42 @@ import { connect } from 'react-redux';
 import '../App.css';
 
 class Button extends Component {
-
   constructor(props) {
     super(props);
-    this.state = {
-      activeColor: 'black'
-    };
 
-    this.changeColor = this.changeColor.bind(this)
+    this.onChangeColor = this.onChangeColor.bind(this);
   }
 
-  changeColor() {
+  onChangeColor() {
+    let {
+      color, activeColor, animation,
+      setActiveColor, setBgColor, setAnimation
+    } = this.props;
 
-    this.setState({
-      activeColor: this.color // activeColor: ?
-    });
+    if (!animation && color !== activeColor) {
+    setActiveColor(color);
+    setAnimation(true);
 
-    let { color } = this.props;
-    console.log('color', color);
-    this.props.onChangeColor( color );
+      setTimeout(() => {
+        setBgColor(color);
+        setAnimation(false);
+      }, 500)
+    }
   }
+
 
   render() {
-    let { color } = this.props;
-    let { activeColor } = this.state;
+    let { color, animation, activeColor } = this.props;
 
     return (
         <div
-            onClick={this.changeColor}
-            className={color + [activeColor === color ? ' active' : '']}
+            onClick={this.onChangeColor}
+            className={
+              'btn ' +
+              color +
+              [activeColor === color ? ' active' : ''] +
+              [animation ? ' cursor-default' : '']
+            }
         />
     );
   }
@@ -40,17 +47,32 @@ class Button extends Component {
 
 Button.propTypes = {
   // props
-  color: PropTypes.string.isRequired
-};
+  color: PropTypes.string.isRequired,
 
+  //redux
+  activeColor: PropTypes.string.isRequired,
+  animation: PropTypes.bool.isRequired,
+
+  //actions
+  setActiveColor: PropTypes.func.isRequired,
+  setBgColor: PropTypes.func.isRequired,
+  setAnimation: PropTypes.func.isRequired
+};
 
 export default connect(
     state => ({
-      activeColor: state.activeColor
+      activeColor: state.activeColor,
+      animation: state.animation
     }),
     dispatch => ({
-      onChangeColor: (color) => {
-        dispatch({type: 'ACTIVE_COLOR',color});
+      setActiveColor: color => {
+        dispatch({ type: 'SET_ACTIVE_COLOR', color});
+      },
+      setBgColor: color => {
+        dispatch({ type: 'SET_BG_COLOR', color});
+      },
+      setAnimation: payload => {
+        dispatch({ type: 'SET_ANIMATION', payload});
       }
     })
 )(Button);
